c6c5cdec87375bceed45d6018fde524d75e81663
Matthew Ballard <matt@mrb.email>
2024-02-23 14:33:11 -0500

    add cass to test

--- tests/lean/run/zetaDeltaIssue.lean
+++ tests/lean/run/zetaDeltaIssue.lean
@@ -7,3 +7,18 @@ def x : A where a := 37
 @[simp] theorem x_a : x.a = 37 := rfl
 
 def y : B := { x with b := 37, w := by simp }
+
+class foo where
+
+def bar : foo := ⟨⟩
+
+def biz : Prop := True
+
+def boz [foo] : biz := trivial
+
+structure baz where
+  p : biz
+
+example : baz :=
+  { bar with p := boz }
+
