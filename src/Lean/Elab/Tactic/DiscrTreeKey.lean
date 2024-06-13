b9c4cd6c2b7a5b533d4864282dde2d3b1cc392a6
Matthew Ballard <matt@mrb.email>
2024-06-13 15:24:39 -0400

    correct name

--- src/Lean/Elab/Tactic/DiscrTreeKey.lean
+++ src/Lean/Elab/Tactic/DiscrTreeKey.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Matthew Robert Ballard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Thomas Skrivan, Matthew Robert Ballard
+Authors: Tomas Skrivan, Matthew Robert Ballard
 -/
 prelude
 import Init.Tactics
